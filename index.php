630a7cf00b926ed9563265c5af7ea34cb169bf9e
olivmiron <30751217+olivmiron@users.noreply.github.com>
2025-01-22 16:14:35 +0200

    Project fs init

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="/website_resources/design/stylehseets/global.css"></link>
+    <link rel="/website_resources/design/stylehseets/index.css"></link>
+    </head>
+    <body>
+        
+    </body>
+</html>
