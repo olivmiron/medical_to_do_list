d62a49a42a8d3e37248e0a5d117fe24be47c261c
olivmiron <30751217+olivmiron@users.noreply.github.com>
2025-01-22 16:17:23 +0200

    further fs init

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <link rel="/website_resources/design/stylehseets/global.css"></link>
-    <link rel="/website_resources/design/stylehseets/index.css"></link>
+
+        <!-- design -->
+        <link rel="/website_resources/design/stylehseets/global.css"></link>
+        <link rel="/website_resources/design/stylehseets/index.css"></link>
+
+        <!-- logic -->
+        <script src="/website_resources/logic/front_end/global.js"></script>
+        <script src="/website_resources/logic/front_end/index.js"></script>
     </head>
     <body>
         
