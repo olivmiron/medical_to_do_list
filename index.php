b924c2c58cfb1a30fb24872f8b14defbaada5416
olivmiron <mironescu.olivier@gmail.com>
2025-01-24 18:28:26 +0200

    css default, favicon, font

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
         <link rel="/website_resources/design/stylehseets/global.css"></link>
         <link rel="/website_resources/design/stylehseets/index.css"></link>
 
+        // link to the favicon
+        <link rel="icon" href="/website_resources/design/media/favicon.ico" type="image/x-icon">
+
         <!-- logic -->
         <script src="/website_resources/logic/front_end/global.js"></script>
         <script src="/website_resources/logic/front_end/index.js"></script>
