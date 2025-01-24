bf278c7adb80b48100e5754d5b2e65d6fbb0b082
olivmiron <mironescu.olivier@gmail.com>
2025-01-24 20:57:51 +0200

    ni

--- website_resources/logic/back_end/core/sign_in_square.php
+++ website_resources/logic/back_end/core/sign_in_square.php
@@ -2,7 +2,9 @@
     <div id="sign_in_square">
         <script src="https://accounts.google.com/gsi/client" async defer></script>
         <span>You are not signed in</span>
-        <br/>
+        
+        <div class="spacer_xl"></div>
+
         <div id="g_id_onload"
             data-client_id="64629051096-v5i6qsdt1mccdd8qimne5v1496o839tj.apps.googleusercontent.com"
             data-callback="handleCredentialResponse">
