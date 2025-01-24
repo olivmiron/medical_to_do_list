faa42b12e4124b88e7c83df592280285d01b4e7c
olivmiron <mironescu.olivier@gmail.com>
2025-01-24 19:47:54 +0200

    ni

--- index.php
+++ index.php
@@ -16,11 +16,6 @@
         <!-- logic -->
         <script src="/website_resources/logic/front_end/global.js"></script>
         <script src="/website_resources/logic/front_end/index.js"></script>
-
-        <?php if(!$_SESSION["logged_in"]) { ?>
-            <script src="https://apis.google.com/js/platform.js" async defer></script>
-            <meta name="google-signin-client_id" content="64629051096-v5i6qsdt1mccdd8qimne5v1496o839tj.apps.googleusercontent.com">
-        <?php } ?>
     </head>
     <body>
         <?php include $_SERVER['DOCUMENT_ROOT'] . "/website_resources/logic/back_end/core/initial_state_checker.php" ; ?>
@@ -28,7 +23,13 @@
         <br/>
         <?php echo var_export($_SESSION["logged_in"]); ?>
         <?php if(!$_SESSION["logged_in"]) { ?>
-            <div class="g-signin2" data-onsuccess="onSignIn"></div>
+            <script src="https://accounts.google.com/gsi/client" async defer></script>
+            <div id="g_id_onload"
+                data-client_id="64629051096-v5i6qsdt1mccdd8qimne5v1496o839tj.apps.googleusercontent.com"
+                data-ux_mode="redirect"
+                data-login_uri="<?php echo $_SERVER['DOCUMENT_ROOT'];?>">
+            </div>
+            <div class="g_id_signin" data-type="standard"></div>
         <?php } ?>
     </body>
 </html>
