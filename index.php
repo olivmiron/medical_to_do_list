38e7f7becc3339f79d179fcb2420cbce9d7445ea
olivmiron <mironescu.olivier@gmail.com>
2025-01-24 19:50:09 +0200

    ni

--- index.php
+++ index.php
@@ -26,8 +26,7 @@
             <script src="https://accounts.google.com/gsi/client" async defer></script>
             <div id="g_id_onload"
                 data-client_id="64629051096-v5i6qsdt1mccdd8qimne5v1496o839tj.apps.googleusercontent.com"
-                data-ux_mode="redirect"
-                data-login_uri="<?php echo $_SERVER['SERVER_NAME'];?>">
+                data-callback="handleCredentialResponse">
             </div>
             <div class="g_id_signin" data-type="standard"></div>
         <?php } ?>
