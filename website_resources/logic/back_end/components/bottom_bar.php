c6210dec3cf19e8ccdbe2cdeebf7491d363f4d0b
olivmiron <mironescu.olivier@gmail.com>
2025-01-24 22:58:32 +0200

    top and bottom bar further init

--- website_resources/logic/back_end/components/bottom_bar.php
+++ website_resources/logic/back_end/components/bottom_bar.php
@@ -1,3 +1,5 @@
 <div id="bottom_bar" class="top_bottom_bar">
-
+    <div class="bottom_bar_button bottom_bar_button_selected"><img src="/website_resources/design/media/icons/patients.png"/></div>
+    <div class="bottom_bar_button"><img src="/website_resources/design/media/icons/to_do.png"/></div>
+    <div class="bottom_bar_button"><img src="/website_resources/design/media/icons/private_to_do.png"/></div>
 </div>
--- website_resources/logic/back_end/components/top_bar.php
+++ website_resources/logic/back_end/components/top_bar.php
@@ -1,2 +1,5 @@
 <div id="top_bar" class="top_bottom_bar">
+    <div id="top_bar_title">
+        <span>Medical to do list</span>
+    </div>
 </div>
