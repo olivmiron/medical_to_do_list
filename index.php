dbe3d541124e86e68d5d8cbb5b9b0f8ae14d95ee
olivmiron <mironescu.olivier@gmail.com>
2025-01-24 20:51:35 +0200

    ni

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <head>
 
         <!-- design -->
-        <link rel="website_resources/design/stylehseets/global.css"></link>
-        <link rel="website_resources/design/stylehseets/index.css"></link>
+        <link rel="stylesheet" type="text/css" href="/website_resources/design/stylehseets/global.css"></link>
+        <link rel="stylesheet" type="text/css" href="/website_resources/design/stylehseets/index.css"></link>
 
         <!-- favicon -->
         <link rel="icon" href="website_resources/design/media/favicon.ico">
@@ -14,8 +14,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <!-- logic -->
-        <script src="website_resources/logic/front_end/global.js"></script>
-        <script src="website_resources/logic/front_end/index.js"></script>
+        <script src="/website_resources/logic/front_end/global.js"></script>
+        <script src="/website_resources/logic/front_end/index.js"></script>
     </head>
     <body>
         <?php include $_SERVER['DOCUMENT_ROOT'] . "/website_resources/logic/back_end/core/initial_state_checker.php" ; ?>
