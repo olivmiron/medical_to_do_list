2a2230e21dc20bf57ab2ed38e04d70c9885a0573
olivmiron <mironescu.olivier@gmail.com>
2025-01-25 20:16:47 +0200

    ni

--- website_resources/logic/back_end/website_pages/view_screen.php
+++ website_resources/logic/back_end/website_pages/view_screen.php
@@ -4,7 +4,7 @@
             $page_uri = $pages_uris[$page_name];
     ?>
 
-        <div class="app_view_screen_page <?php echo $pages_arrangement[$page_name][1]; ?>" style="background-color: #<?php echo rand(10, 99);echo rand(10, 99);echo rand(10, 99); ?>">
+        <div class="app_view_screen_page <?php echo $pages_arrangement[$page_name][1]; ?>">
             <?php include $_SERVER["DOCUMENT_ROOT"] . "/" . $page_uri; ?>
         </div>
 
