31b28faa712d8a906ff6d7a2d62596285d71cff9
olivmiron <mironescu.olivier@gmail.com>
2025-01-27 12:11:05 +0200

    ni 3

--- website_resources/logic/back_end/core/global_requirements_dependencies/log_in_checker.php
+++ website_resources/logic/back_end/core/global_requirements_dependencies/log_in_checker.php
@@ -8,7 +8,7 @@
 
 
     if (isset($_COOKIE["log_in_cookie"])) {
-        $log_in_cookie = json_decode(unserialize($_COOKIE["log_in_cookie"]));
+        $log_in_cookie = unserialize($_COOKIE["log_in_cookie"]);
         $user_id = $log_in_cookie["user_id"];
         $user_token = $log_in_cookie["user_token"];
 
