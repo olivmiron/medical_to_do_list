6678f1a734c705aefb2a06c6ed0a67841259ea68
olivmiron <mironescu.olivier@gmail.com>
2025-01-27 21:45:48 +0200

    ni

--- website_resources/logic/back_end/components/top_bar.php
+++ website_resources/logic/back_end/components/top_bar.php
@@ -6,7 +6,7 @@
     <div id="top_bar_middle"></div>
     <div id="top_bar_right">
         <?php if($_SESSION["logged_in"]) { ?>
-        <img id="top_bar_user_picture" src="content_resources/user_pictures/<?php echo $_SESSION["user_id"]; ?>.png"/>
+        <img id="top_bar_user_picture" src="/content_resources/user_pictures/<?php echo $_SESSION["user_id"]; ?>.png"/>
         <?php } ?>
     </div>
 </div>
