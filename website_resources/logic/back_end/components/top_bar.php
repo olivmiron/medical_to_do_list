275c483ddd6421e4ec6415682605e7f7beca4083
olivmiron <mironescu.olivier@gmail.com>
2025-01-27 21:45:34 +0200

    ni

--- website_resources/logic/back_end/components/top_bar.php
+++ website_resources/logic/back_end/components/top_bar.php
@@ -2,4 +2,11 @@
     <div id="top_bar_title">
         <span>Medical to do list</span>
     </div>
+    <div id="top_bar_left"></div>
+    <div id="top_bar_middle"></div>
+    <div id="top_bar_right">
+        <?php if($_SESSION["logged_in"]) { ?>
+        <img id="top_bar_user_picture" src="content_resources/user_pictures/<?php echo $_SESSION["user_id"]; ?>.png"/>
+        <?php } ?>
+    </div>
 </div>
