ed2f626188017c42f152db1ca5cd6a210bffe290
olivmiron <mironescu.olivier@gmail.com>
2025-01-27 20:15:07 +0200

    ni

--- website_resources/logic/front_end/global.js
+++ website_resources/logic/front_end/global.js
@@ -96,8 +96,8 @@ function change_page(page_name) {
 
 function open_bottom_sheet() {
     var bottom_sheet = document.getElementById("bottom_drag_sheet_container");
-    bottom_sheet.classList.add("bottom_drag_sheet_active");
     bottom_sheet.style.display = "block";
+    bottom_sheet.classList.add("bottom_drag_sheet_active");
 
 }
 
