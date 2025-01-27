84126c8ced3fa83c85b2a038cc3bd87c32251577
olivmiron <mironescu.olivier@gmail.com>
2025-01-27 19:31:40 +0200

    ni

--- website_resources/logic/back_end/components/bottom_drag_sheet.php
+++ website_resources/logic/back_end/components/bottom_drag_sheet.php
@@ -1,9 +1,12 @@
+<div id="bottom_drag_sheet_container"></div> 
+
 <div id="bottom_drag_sheet">
     <div id="bottom_drag_sheet_handle"></div>
     <div id="bottom_drag_sheet_content">
 
     </div>
 </div>
+
 
 <div id="bottom_drag_sheet_stored_templates">
 
