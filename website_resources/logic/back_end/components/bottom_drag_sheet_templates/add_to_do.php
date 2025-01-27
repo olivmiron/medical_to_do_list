bf7d6af46e20c411d89154fb1046ec54d657ab76
olivmiron <mironescu.olivier@gmail.com>
2025-01-27 20:49:58 +0200

    ni

--- website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_to_do.php
+++ website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_to_do.php
@@ -3,11 +3,11 @@
     <div class="spacer_large"></div>
 
     
-    <div class="bottom_sheet_title"><span>Title</span></div>
-    <input id="add_to_do_sub_title" type="text"></input>
+    <div class="bottom_sheet_sub_title"><span>Title</span></div>
+    <input id="add_to_do_title_input" type="text"></input>
     
     <div class="spacer_medium"></div>
     
     <div class="bottom_sheet_sub_title"><span>Description / details</span></div>
-    <textarea id="add_to_do_description"></textarea>
+    <textarea id="add_to_do_description_input"></textarea>
 </div>
