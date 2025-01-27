017727190b8da379e07de3135e2de7544d633b86
olivmiron <mironescu.olivier@gmail.com>
2025-01-27 20:48:14 +0200

    add_to_to_bottom_sheet_part further init, custom elements further init

--- website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_to_do.php
+++ website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_to_do.php
@@ -1,8 +1,13 @@
-<div>
+<div class="bottom_sheet_component">
     <div class="bottom_sheet_title"><span>Add to do</span></div>
-    <div class="spacer_medium"></div>
+    <div class="spacer_large"></div>
 
     
     <div class="bottom_sheet_title"><span>Title</span></div>
-    <input id="add_to_do_title" type="text" placeholder="Title"></input>
+    <input id="add_to_do_title" type="text"></input>
+    
+    <div class="spacer_medium"></div>
+    
+    <div class="bottom_sheet_title"><span>Description / details</span></div>
+    <textarea id="add_to_do_description"></textarea>
 </div>
