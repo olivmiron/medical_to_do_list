0bb36d4ad26587c75b4d055ad3200399219d32a4
olivmiron <mironescu.olivier@gmail.com>
2025-01-28 20:25:56 +0200

    ni

--- website_resources/logic/back_end/components/top_bar.php
+++ website_resources/logic/back_end/components/top_bar.php
@@ -3,10 +3,10 @@
         <span><?php echo $top_bar_title; ?></span>
     </div>
     <div id="top_bar_left">
-            <img id="top_bar_action_button" src="<?php if(isset($pages_top_bar_action_buttons[$initial_page][0])) {echo $pages_top_bar_action_buttons[$initial_page][1];}; ?>" onclick="<?php if(isset($pages_top_bar_action_buttons[$initial_page][0])) {echo $pages_top_bar_action_buttons[$initial_page][0];}; ?>" <?php if($_SESSION["logged_in"]) {echo "style='display: none;'";} ?>/>
+            <img id="top_bar_action_button" src="<?php if(isset($pages_top_bar_action_buttons[$initial_page][0])) {echo $pages_top_bar_action_buttons[$initial_page][1];}; ?>" onclick="<?php if(isset($pages_top_bar_action_buttons[$initial_page][0])) {echo $pages_top_bar_action_buttons[$initial_page][0];}; ?>" <?php if(!$_SESSION["logged_in"]) {echo "style='display: none;'";} ?>/>
     </div>
     <div id="top_bar_middle"></div>
     <div id="top_bar_right">
-            <img id="top_bar_user_picture" src="/content_resources/user_pictures/<?php echo $_SESSION["user_id"]; ?>.png" <?php if($_SESSION["logged_in"]) {echo "style='display: none;'";} ?>/>
+            <img id="top_bar_user_picture" src="/content_resources/user_pictures/<?php echo $_SESSION["user_id"]; ?>.png" <?php if(!$_SESSION["logged_in"]) {echo "style='display: none;'";} ?>/>
     </div>
 </div>
