bbac2f017783d18befa31c72e7ab39587001fee6
olivmiron <mironescu.olivier@gmail.com>
2025-01-28 19:08:06 +0200

    ni

--- website_resources/logic/back_end/website_pages/fetch_page.php
+++ website_resources/logic/back_end/website_pages/fetch_page.php
@@ -1,11 +1,17 @@
-<?php $initial_load = false; require $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/core/global_requirements.php";
+<?php // $initial_load = false; require $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/core/global_requirements.php";
 
-$page_to_load = $_GET["page"];
-
-// js handles views; just load page from folder if $page_to_load is in $pages_array
-if(in_array($page_to_load, $pages_array)) {
-    include $_SERVER["DOCUMENT_ROOT"] . "/" . $pages_uris[$page_to_load];
+if($_SESSION["logged_in"]) {
+echo "o";
 }
+else {echo "x";}
+
+
+// $page_to_load = $_GET["page"];
+
+// // js handles views; just load page from folder if $page_to_load is in $pages_array
+// if(in_array($page_to_load, $pages_array)) {
+//     include $_SERVER["DOCUMENT_ROOT"] . "/" . $pages_uris[$page_to_load];
+// }
 
 
 ?>
