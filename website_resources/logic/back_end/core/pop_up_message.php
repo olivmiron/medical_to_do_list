93e8a04370b19be2c06e2e7b50169642c9364ca0
olivmiron <mironescu.olivier@gmail.com>
2025-01-29 21:51:28 +0200

    ni

--- website_resources/logic/back_end/core/pop_up_message.php
+++ website_resources/logic/back_end/core/pop_up_message.php
@@ -1,3 +1,3 @@
-<div id="pop_up_message" class="pop_up_message_visible pop_up_message_error">
+<div id="pop_up_message" class=""><!-- available classes: pop_up_message_visible, pop_up_message_error -->
     <span>Message</span>
 </div>
