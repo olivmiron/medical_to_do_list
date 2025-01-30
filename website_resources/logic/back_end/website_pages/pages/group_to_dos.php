cbd4417ced40908d716b0ab87aaf5248e8c776be
olivmiron <mironescu.olivier@gmail.com>
2025-01-30 19:48:46 +0200

    added group to dos

--- website_resources/logic/back_end/website_pages/pages/group_to_dos.php
+++ website_resources/logic/back_end/website_pages/pages/group_to_dos.php
@@ -4,6 +4,8 @@ if($_SESSION["logged_in"]) {
     $_SESSION["loaded_pages"]["group_to_dos"] = true; 
 ?>
 <div id="view_screen_page__group_to_dos__content">
-    Group to dos
+    
+<?php $personal_to_dos = false;include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php"; ?>
+
 </div>
 <?php } else {if($initial_load) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/not_logged_in.html';}} ?>
