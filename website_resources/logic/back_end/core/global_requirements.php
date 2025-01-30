123367af298d8bedeae806c2d7a691883a877571
olivmiron <mironescu.olivier@gmail.com>
2025-01-30 19:32:13 +0200

    ni

--- website_resources/logic/back_end/core/global_requirements.php
+++ website_resources/logic/back_end/core/global_requirements.php
@@ -79,11 +79,12 @@ if($initial_load) {
         }
     }
 
-}
-else{
+    
     if(!$_SESSION["logged_in"]) {
         include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/other/not_logged_in.html";
     }
 }
+// else{
+// }
 
 ?>
