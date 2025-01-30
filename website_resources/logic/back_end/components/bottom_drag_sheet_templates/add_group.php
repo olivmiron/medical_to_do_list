3c72900d04b823e55d17d09c887e3386ea9afdd1
olivmiron <mironescu.olivier@gmail.com>
2025-01-30 20:51:49 +0200

    ni

--- website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_group.php
+++ website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_group.php
@@ -1,3 +1,6 @@
 <div id="add_group__bottom_sheet_action_template" class="bottom_sheet_component">
+    <div class="bottom_sheet_title"><span>Add group</span></div>
+    <div class="spacer_large"></div>
+
     <?php include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/other/work_in_progress.html"; ?>
 </div>
--- website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_patient.php
+++ website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_patient.php
@@ -1,3 +1,7 @@
 <div id="add_patient__bottom_sheet_action_template" class="bottom_sheet_component">
+    <div class="bottom_sheet_title"><span>Add patient</span></div>
+    <div class="spacer_large"></div>
+
+
     <?php include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/other/work_in_progress.html"; ?>
 </div>
