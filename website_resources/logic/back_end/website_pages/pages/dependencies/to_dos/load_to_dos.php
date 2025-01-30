73836777b871f4c1c40974948468d32ce2bfe8c5
olivmiron <mironescu.olivier@gmail.com>
2025-01-30 19:56:46 +0200

    ni

--- website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php
+++ website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php
@@ -8,11 +8,11 @@ if($personal_to_dos) {$personal_or_group_id = 0;}
 else {$personal_or_group_id = $_SESSION["default_group_id"];}
 
 // if need to load grou to dos but no default_group_id, then throw static message to add/create a group
-if(!($personal_to_dos or !empty($personal_or_group_id))) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/add_a_group_first.html';echo $personal_or_group_id . "x";}
+if(!($personal_to_dos or !empty($personal_or_group_id))) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/add_a_group_first.html';}
 else {
 
 
-    $stmt = $conn->prepare("SELECT id, title, description, date_created FROM to_dos WHERE creator_user_id = ? AND personal_or_group_id = ? ORDER BY date_created DESC LIMIT 10");
+    $stmt = $conn->prepare("SELECT * FROM to_dos WHERE creator_user_id = ? AND personal_or_group_id = ? ORDER BY date_created DESC LIMIT 10 OFFSET 0");
     $stmt->bind_param("ii", $creator_user_id, $personal_or_group_id);
     $stmt->execute();
     $result = $stmt->get_result();
@@ -26,6 +26,8 @@ else {
         $to_do_html = str_replace(
             [
                 '{{to_do_id}}', 
+                "{{to_do_done_or_not}}", 
+                "data-to_do_done='0'",
                 '{{to_do_date_created}}', 
                 '{{to_do_creator_name}}',
                 '{{to_do_title}}', 
@@ -34,6 +36,8 @@ else {
             ],
             [
                 $row['id'], 
+                ($row["to_do_done"] == 0 ? "to_do_item_not_done" : "to_do_item_done" ),
+                ($row["to_do_done"] == 0 ? "data-to_do_done='0'" : "data-to_do_done='1'" ),
                 date('d M Y H:i', strtotime($row['date_created'])), 
                 $_SESSION["user_name"],
                 $row['title'], 
