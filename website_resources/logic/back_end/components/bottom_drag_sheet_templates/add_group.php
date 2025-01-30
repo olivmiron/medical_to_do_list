126ca0a9172e4dd13dc014c1f49b509b87d84d49
olivmiron <mironescu.olivier@gmail.com>
2025-01-30 20:47:57 +0200

    ni

---
+++ website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_group.php
@@ -0,0 +1,3 @@
+<div id="add_group__bottom_sheet_action_template" class="bottom_sheet_component">
+    <?php include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/other/work_in_progress.html"; ?>
+</div>
