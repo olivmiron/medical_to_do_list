2e5895f5d70381f9ef5c33e763ffc00e764b1786
olivmiron <mironescu.olivier@gmail.com>
2025-01-30 19:58:16 +0200

    ni

--- website_resources/logic/back_end/website_pages/pages/personal_to_dos.php
+++ website_resources/logic/back_end/website_pages/pages/personal_to_dos.php
@@ -8,4 +8,4 @@ if($_SESSION["logged_in"]) {
 <?php $personal_to_dos = true;include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php"; ?>
     
 </div>
-<?php } else {if($initial_load) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/not_logged_in.html';}} ?>
+<?php } else {if($initial_load) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/not_logged_in.html';echo "x";}} ?>
