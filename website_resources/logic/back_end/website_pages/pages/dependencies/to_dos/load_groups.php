a89c163dd0aabda06f5bf67617d177e10a6db83c
olivmiron <mironescu.olivier@gmail.com>
2025-01-31 22:27:14 +0200

    ni

---
+++ website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_groups.php
@@ -0,0 +1,48 @@
+<?php
+if(!$initial_load) {require $_SERVER['DOCUMENT_ROOT'] . "/website_resources/logic/back_end/core/global_requirements.php";}
+require $_SERVER['DOCUMENT_ROOT'] . "/website_resources/logic/back_end/core/database_connect.php";
+
+$user_id = $_SESSION['user_id'];
+
+    $stmt = $conn->prepare("
+        SELECT g.id, g.group_name 
+        FROM groups g 
+        INNER JOIN groups_members gm ON g.id = gm.group_id 
+        WHERE gm.member_id = ? 
+        ORDER BY g.date_created DESC 
+        LIMIT 10 OFFSET 0
+    ");
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+
+    $loaded_group_rows = 0;
+    while ($row = $result->fetch_assoc()) {
+        $group_row_template = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/group_row.html');
+        
+    
+        $group_row_html = str_replace(
+            [
+                "{{group_id}}", 
+                "{{default_group_or_not}}", 
+                "{{group_name}}"
+            ],
+            [
+                $row['id'], 
+                ($_SESSION["default_group_id"] == $row["id"] ? "settings_group_row_selected" : "" ),
+                $row["group_name"]
+            ],
+            $group_row_template
+        );
+        
+        echo $group_row_html;
+
+        $loaded_group_rows++;
+    }
+
+    if($loaded_to_dos < 10) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/query_end.html';}
+    
+    $stmt->close();
+    $conn->close();
+?>
--- website_resources/logic/back_end/website_pages/pages/settings.php
+++ website_resources/logic/back_end/website_pages/pages/settings.php
@@ -6,11 +6,8 @@ if($_SESSION["logged_in"]) {
 <div class="page_title"><span>Your groups</span></div>
 <div class="spacer_large"></div>
 <div id="view_screen_page__settings__content">
-    <div class="settings_group_row">
-        <div class="checkbox_box"><img src="/website_resources/design/media/icons/global/check_mark.png"></div>
-        <span>Some group</span>
-    </div>
-    <?php include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/other/work_in_progress.html"; ?>
+    
+    <?php $personal_to_dos = true;include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php"; ?>
 
 </div>
 <?php } else {if($initial_load) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/not_logged_in.html';}} ?>
