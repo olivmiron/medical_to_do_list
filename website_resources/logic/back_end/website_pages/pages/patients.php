967c94a2d9ebf466964ef4ec7ba5105f2bd1d48f
olivmiron <mironescu.olivier@gmail.com>
2025-01-31 23:38:04 +0200

    ni

--- website_resources/logic/back_end/website_pages/pages/patients.php
+++ website_resources/logic/back_end/website_pages/pages/patients.php
@@ -3,11 +3,11 @@
 if($_SESSION["logged_in"]) {
     $_SESSION["loaded_pages"]["patients"] = true; 
 
-    // include $_SERVER['DOCUMENT_ROOT'] . "/website_resources/logic/back_end/website_pages/pages/dependencies/groups/get_group_name.php";
-    // $group_data = json_decode($group_name, true);
+    include $_SERVER['DOCUMENT_ROOT'] . "/website_resources/logic/back_end/website_pages/pages/dependencies/groups/get_group_name.php";
+    $group_data = json_decode($group_name, true);
 ?>
 <div class="page_title"><span>
-    <?php // echo htmlspecialchars($group_data['group_name'] ?? 'Group'); ?>
+    <?php echo htmlspecialchars($group_data['group_name'] ?? 'Group'); ?>
     <span>'s Patients</span>
 </div>
 <div class="spacer_large"></div>
