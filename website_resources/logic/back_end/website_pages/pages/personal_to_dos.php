22692b2f0ec40244a6c0d9d50cab58db24091163
olivmiron <mironescu.olivier@gmail.com>
2025-01-31 21:27:34 +0200

    ni

--- website_resources/logic/back_end/website_pages/pages/personal_to_dos.php
+++ website_resources/logic/back_end/website_pages/pages/personal_to_dos.php
@@ -4,6 +4,7 @@ if($_SESSION["logged_in"]) {
     $_SESSION["loaded_pages"]["personal_to_dos"] = true; 
 ?>
 <div class="page_title"><span>Your To Dos</span></div>
+<div class="spacer_large"></div>
 <div id="view_screen_page__personal_to_dos__content">
 
     <?php $personal_to_dos = true;include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php"; ?>
