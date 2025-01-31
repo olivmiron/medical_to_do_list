6e2e922d44aa16d51807d5a49b8f13e4c5063c40
olivmiron <mironescu.olivier@gmail.com>
2025-01-31 21:33:16 +0200

    ni

--- website_resources/logic/back_end/website_pages/pages/settings.php
+++ website_resources/logic/back_end/website_pages/pages/settings.php
@@ -6,7 +6,10 @@ if($_SESSION["logged_in"]) {
 <div class="page_title"><span>Your groups</span></div>
 <div class="spacer_large"></div>
 <div id="view_screen_page__settings__content">
-
+    <div class="settings_group_row">
+        <div class="checkbox_box"><img src="/website_resources/design/media/icons/global/check_mark.png"></div>
+        <span>Some group</span>
+    </div>
     <?php include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/other/work_in_progress.html"; ?>
 
 </div>
