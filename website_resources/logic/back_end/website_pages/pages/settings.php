bc4e0cb547b2419b8c8f9131adcd29bb1eea4c35
olivmiron <mironescu.olivier@gmail.com>
2025-01-31 22:28:10 +0200

    ni

--- website_resources/logic/back_end/website_pages/pages/settings.php
+++ website_resources/logic/back_end/website_pages/pages/settings.php
@@ -6,8 +6,8 @@ if($_SESSION["logged_in"]) {
 <div class="page_title"><span>Your groups</span></div>
 <div class="spacer_large"></div>
 <div id="view_screen_page__settings__content">
-    
-    <?php $personal_to_dos = true;include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php"; ?>
+
+    <?php $personal_to_dos = true;include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_groups.php"; ?>
 
 </div>
 <?php } else {if($initial_load) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/not_logged_in.html';}} ?>
