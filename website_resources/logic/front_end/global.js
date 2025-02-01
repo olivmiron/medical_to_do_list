9967254551bcae63134d65f60d1e2bd34acf23e0
olivmiron <mironescu.olivier@gmail.com>
2025-02-01 22:07:37 +0200

    ni

--- website_resources/logic/front_end/global.js
+++ website_resources/logic/front_end/global.js
@@ -312,11 +312,11 @@ function stopDragging() {
 // Initialize dragging when DOM is loaded
 // document.addEventListener('DOMContentLoaded', initBottomSheetDrag);
 
-document.addEventListener('DOMContentLoaded', () => {
-    initBottomSheetDrag();
-});
+// document.addEventListener('DOMContentLoaded', () => {
+    
+// });
 
-
+initBottomSheetDrag();
 
 
 // pages functions
