240a21d6e4a972c1d41a30ea132b582f29dbe894
olivmiron <mironescu.olivier@gmail.com>
2025-02-01 15:35:17 +0200

    ni

--- website_resources/logic/front_end/global.js
+++ website_resources/logic/front_end/global.js
@@ -447,38 +447,6 @@ function select_group_as_default(group_id) {
 
 
 
-function load_more_groups(button) {
-    let groups_initially_loaded = document.getElementById("view_screen_page__settings__content").querySelectorAll(".settings_group_row").length;
-
-    let data = { groups_offset: groups_initially_loaded };
-
-    fetch('/website_resources/logic/back_end/website_pages/pages/dependencies/groups/load_groups.php?' + new URLSearchParams(data), {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.text())
-    .then(html => {
-        let groups_container = document.getElementById("view_screen_page__settings__content");
-
-        groups_container.innerHTML += html;
-
-        let groups_loaded = groups_container.querySelectorAll(".settings_group_row").length - groups_initially_loaded;
-
-        console.log("groups initially loaded: " + groups_initially_loaded + ", groups finally loaded: " + groups_loaded);
-
-        // Check if there are more to-dos to load
-        if (groups_loaded < 10) {
-            button.closest('.load_more_button_container').style.display = 'none';
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        show_pop_up_message('Please try again later', true);
-    });
-}
-
 
 
 
