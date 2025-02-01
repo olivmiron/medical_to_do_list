164923b85afc7c75dfb83a48f0babb584d9bc4a8
olivmiron <mironescu.olivier@gmail.com>
2025-02-01 14:06:53 +0200

    ni

--- website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php
+++ website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php
@@ -4,16 +4,17 @@ require $_SERVER['DOCUMENT_ROOT'] . "/website_resources/logic/back_end/core/data
 
 $creator_user_id = $_SESSION['user_id'];
 
-if($personal_to_dos) {$personal_or_group_id = 0;}
+if($_GET["personal_to_dos"]) {$personal_or_group_id = 0;}
 else {$personal_or_group_id = $_SESSION["default_group_id"];}
 
 // if need to load grou to dos but no default_group_id, then throw static message to add/create a group
 if(!($personal_to_dos or !empty($personal_or_group_id))) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/add_a_group_first.html';}
 else {
 
+    $to_dos_offset = (int) $_GET["to_dos_offset"];
 
-    $stmt = $conn->prepare("SELECT * FROM to_dos WHERE creator_user_id = ? AND personal_or_group_id = ? ORDER BY date_created DESC LIMIT 10 OFFSET 0");
-    $stmt->bind_param("ii", $creator_user_id, $personal_or_group_id);
+    $stmt = $conn->prepare("SELECT * FROM to_dos WHERE creator_user_id = ? AND personal_or_group_id = ? ORDER BY date_created DESC LIMIT 10 OFFSET ?");
+    $stmt->bind_param("iii", $creator_user_id, $personal_or_group_id, $to_dos_offset);
     $stmt->execute();
     $result = $stmt->get_result();
     
