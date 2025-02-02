5aa3bfbbd77d90a8a3dc141d507f06c1fd29a7ab
olivmiron <mironescu.olivier@gmail.com>
2025-02-02 16:21:49 +0200

    ni

--- website_resources/logic/back_end/core/pop_up_message.php
+++ website_resources/logic/back_end/core/pop_up_message.php
@@ -1,7 +1,7 @@
 <div id="pop_up_message" class=""><!-- available classes: pop_up_message_visible, pop_up_message_error -->
     <span>Message</span>
     <div id="pop_up_message_buttons">
-        <div  class="pop_up_message_buttons_button" onclick="pop_up_message_get_confirmation_decline()"><span>No</span></div>
+        <div class="pop_up_message_buttons_button" onclick="pop_up_message_get_confirmation_decline()"><span>No</span></div>
         <div id="pop_up_message_yes_button" class="pop_up_message_buttons_button" onclick=""><span>Yes</span></div>
     </div>
 </div>
