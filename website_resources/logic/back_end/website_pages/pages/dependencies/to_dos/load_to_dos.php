ca3686c5da3e3117a090fb0470cbb07cfe8639f2
olivmiron <30751217+olivmiron@users.noreply.github.com>
2025-02-03 11:44:54 +0200

    ni

--- website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php
+++ website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php
@@ -18,7 +18,7 @@ else {
 
     if($personal_to_dos) {
         $stmt = $conn->prepare("SELECT * FROM to_dos WHERE creator_user_id = ? AND personal_or_group_id = 0 ORDER BY date_created, id DESC LIMIT 10 OFFSET ?");
-        $stmt->bind_param("ii", $creator_user_id $to_dos_offset);
+        $stmt->bind_param("ii", $creator_user_id, $to_dos_offset);
     }
     else {
         $stmt = $conn->prepare("SELECT * FROM to_dos WHERE personal_or_group_id = ? AND personal_or_group_id != 0 ORDER BY date_created, id DESC LIMIT 10 OFFSET ?");
