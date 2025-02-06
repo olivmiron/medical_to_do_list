fbb55169dd08e234dfbc40e424d3d7aea50226c4
olivmiron <mironescu.olivier@gmail.com>
2025-02-06 23:19:00 +0200

    ni

--- website_resources/logic/back_end/website_pages/pages/patients.php
+++ website_resources/logic/back_end/website_pages/pages/patients.php
@@ -13,7 +13,33 @@ if($_SESSION["logged_in"]) {
 
     <br/>
 
-    <?php include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/website_pages/pages/dependencies/patients/patient.html'; ?>
+    <?php 
+    $patient_template = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/website_pages/pages/dependencies/patients/patient.html');
+    $patient_template_populated = str_replace(
+        [
+            '{{patient_id}}',
+            '{{admission_date}}',
+            '{{caregiver_name}}',
+            '{{delete_option_available_or_not}}',
+            '{{patient_identification}}',
+            '{{patient_age}}',
+            '{{patient_location}}',
+            '{{patient_description}}'
+        ], 
+        [
+            rand(1, 1000),
+            date('Y-m-d'),
+            'Mironescu Olivier', 
+            '', 
+            'Mary J.', 
+            rand(1, 100), 
+            'P4S2',
+            'A brief description of the patient.'
+        ], 
+        $patient_template
+    );
+    echo $patient_template_populated;
+    ?>
 
 </div>
 <?php } else {if($initial_load) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/not_logged_in.html';}} ?>
