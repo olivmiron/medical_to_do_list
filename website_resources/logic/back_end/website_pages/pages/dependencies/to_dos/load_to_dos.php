9f1030b1a531842ae84f9823ba60e55a45f2377d
olivmiron <mironescu.olivier@gmail.com>
2025-02-16 01:05:03 +0200

    ni

--- website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php
+++ website_resources/logic/back_end/website_pages/pages/dependencies/to_dos/load_to_dos.php
@@ -8,11 +8,11 @@ $creator_user_id = $_SESSION['user_id'];
 $personal_to_dos = $_GET["personal_to_dos"];
 $to_dos_offset = (int) $_GET["to_dos_offset"];
 
-if($personal_to_dos) {$personal_or_group_id = 0;}
+if($personal_to_dos == "true") {$personal_or_group_id = 0;}
 else {$personal_or_group_id = $_SESSION["default_group_id"];}
 
 // if need to load grou to dos but no default_group_id, then throw static message to add/create a group
-if(!($personal_to_dos or !empty($personal_or_group_id))) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/add_a_group_first.html';}
+if(!($personal_to_dos == "true" or !empty($personal_or_group_id))) {include $_SERVER['DOCUMENT_ROOT'] . '/website_resources/logic/back_end/other/add_a_group_first.html';}
 else {
 
 
