4ee3bc491d6b5a1179054cdbe43b10be318e72d0
olivmiron <mironescu.olivier@gmail.com>
2025-02-15 17:40:25 +0200

    ni

--- website_resources/logic/back_end/components/join_group_qr_code.php
+++ website_resources/logic/back_end/components/join_group_qr_code.php
@@ -1,5 +1,5 @@
 <div id="join_group_qr_code" class="floating_box floating_box_hidden">
-    <div class="floating_box_close_button" onclick="close_floating_box(this)"><img src=""/></div>
+    <div class="floating_box_close_button" onclick="close_floating_box(this)"><img src="/website_resources/design/media/icons/global/close.png"/></div>
     <span class="floating_box_title">Invite QR code</span>
     <div id="join_group_qr_code_code">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/2560px-QR_code_for_mobile_English_Wikipedia.svg.png"/>
