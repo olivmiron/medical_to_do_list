78076c9c46c23b2dea4528e5d52a2f265c0c0d6b
olivmiron <mironescu.olivier@gmail.com>
2025-02-21 09:01:18 +0200

    to_do_due_date_picker init

--- website_resources/logic/back_end/components/bottom_drag_sheet_templates/create_or_edit_to_do.php
+++ website_resources/logic/back_end/components/bottom_drag_sheet_templates/create_or_edit_to_do.php
@@ -12,6 +12,19 @@
     <textarea id="add_to_do_description_input"></textarea>
     
     <div class="spacer_xxl"></div>
+    
+    <div class="bottom_sheet_sub_title"><span>Due in</span></div>
+
+    <div id="create_or_edit_to_do_due_date_picker" data-due_date="0">
+        <div class="due_date_picker_container">
+            <div class="due_date_picker_pointer"></div>
+            <div class="due_date_picker_dates">
+                <span>|<br/>none</span>
+                <span>|<br/>1 day</span>
+                <span>|<br/>10 days</span>
+            </div>
+        </div>
+    </div>
 
     <div class="big_bottom_sheet_action_button" onclick="create_or_edit_to_do()"><span>Save to do</span></div>
 </div>
