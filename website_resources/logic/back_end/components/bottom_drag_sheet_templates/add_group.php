2def98ccbb1be404b4b41132b1dbb43f921e12b9
olivmiron <mironescu.olivier@gmail.com>
2025-02-27 13:16:19 +0200

    ni

--- website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_group.php
+++
@@ -1,6 +0,0 @@
-<div id="add_group__bottom_sheet_action_template" class="bottom_sheet_component">
-    <div class="bottom_sheet_title"><span>Add group</span></div>
-    <div class="spacer_large"></div>
-
-    <?php include $_SERVER["DOCUMENT_ROOT"] . "/website_resources/logic/back_end/other/work_in_progress.html"; ?>
-</div>
---
+++ website_resources/logic/back_end/components/bottom_drag_sheet_templates/add_media.php
@@ -0,0 +1,23 @@
+<div id="add_media__bottom_sheet_action_template" class="bottom_sheet_component">
+    <div class="bottom_sheet_title"><span>Add content</span></div>
+    <div class="spacer_large"></div>
+
+    
+    <div class="bottom_sheet_sub_title"><span>Title</span></div>
+    <input id="add_to_do_title_input" type="text" oninput="remove_empty_input_class(this)"></input>
+    
+    <div class="spacer_xxl"></div>
+    
+    <div class="bottom_sheet_sub_title"><span>Description / details</span></div>
+    <textarea id="add_to_do_description_input"></textarea>
+    
+    <div class="spacer_xxl"></div>
+    
+    <div class="bottom_sheet_sub_title"><span>Add media</span><span class="add_media_due_date_span"></span></div>
+
+    <div></div>
+    
+    <div class="spacer_xxl"></div>
+
+    <div class="big_bottom_sheet_action_button" onclick="add_media()"><span>Add media</span></div>
+</div>
