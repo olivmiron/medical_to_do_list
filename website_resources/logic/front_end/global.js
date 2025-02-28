c9f4e1665600b74b757055e3255b9072d8ab8615
olivmiron <mironescu.olivier@gmail.com>
2025-02-28 10:25:35 +0200

    ni

--- website_resources/logic/front_end/global.js
+++ website_resources/logic/front_end/global.js
@@ -778,7 +778,7 @@ function load_more_content(to_do_id) {
                     
                     if(data.number_of_elements_loaded > 0) {
                     respective_to_do_element.querySelector(".to_do_content_no_content").querySelector("span").innerText = "";
-                    respective_to_do_element.querySelector(".to_do_content").innerHTML += atob(data.html);
+                    respective_to_do_element.querySelector(".to_do_content").innerHTML += atob(data.content_html);
                     }
 
                     if(data.number_of_elements_loaded < 5) {
