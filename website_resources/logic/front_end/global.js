2260ce0c339c63d97a7b7a1937757fb0c8bbde37
olivmiron <mironescu.olivier@gmail.com>
2025-03-01 22:35:29 +0200

    ni

--- website_resources/logic/front_end/global.js
+++ website_resources/logic/front_end/global.js
@@ -860,7 +860,7 @@ function add_content_to_db() {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(add_content_obj)
+        body: JSON.stringify({title: add_content_obj.title, description: add_content_obj.description, media: add_content_obj.media, to_do_or_paient: add_content_obj.to_do_or_patient, to_do_or_paient_id: add_content_obj.to_do_or_patient_id})
     })
     .then(response => response.json())
     .then(data => {
