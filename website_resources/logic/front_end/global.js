57abc2e8557fa57019f4ed9e8c5c8809363d06a7
olivmiron <mironescu.olivier@gmail.com>
2025-04-12 21:38:22 +0300

    ni

--- website_resources/logic/front_end/global.js
+++ website_resources/logic/front_end/global.js
@@ -4,7 +4,7 @@ import "/website_resources/logic/front_end/core/user_logging.js";
 
 
 // website_parts
-import "/website_resources/logic/front_end/website_parts/top_bottom_bars.js";
+import * as Namespace from "/website_resources/logic/front_end/website_parts/top_bottom_bars.js";
 import "/website_resources/logic/front_end/website_parts/app_view_screen.js";
 import "/website_resources/logic/front_end/website_parts/pages.js";
 
